611f5671435b3ec01cfdd48361ffae06e71a150b
Ari Lerner <arilerner@mac.com>
2016-06-17 10:13:11 -0700

    WIP

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,3 +2,23 @@
 
 cp -R Example/ios/Pods/ .
 mv Pods.xcodeproj Firestack.xcodeproj
+
+echo <<-EOE
+# Uncomment this line to define a global platform for your project
+platform :ios, '8.0'
+
+target 'Example' do
+  # Uncomment this line if you're using Swift or would like to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Example
+  pod 'React', :path => '../node_modules/react-native'
+  pod 'Firestack', :path => './'
+
+  target 'ExampleTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
+EOE > ./Podfile
