b0eedfa3e7744fb5f468b8a229c26935a82ffb27
Ari Lerner <arilerner@mac.com>
2016-08-18 15:35:29 -0700

    Update firestackModule setAt,getAt,updateAt,removeAt, added Log shim

--- android/src/main/java/io/fullstack/firestack/FirestackModule.java
+++ android/src/main/java/io/fullstack/firestack/FirestackModule.java
@@ -3,6 +3,7 @@ package io.fullstack.firestack;
 import android.content.Context;
 import android.util.Log;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -38,6 +39,7 @@ class FirestackModule extends ReactContextBaseJavaModule {
   public FirestackModule(ReactApplicationContext reactContext) {
     super(reactContext);
     this.context = reactContext;
+    Log.d(TAG, "New FirestackModule instance");
   }
 
   @Override
@@ -46,38 +48,53 @@ class FirestackModule extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod
-  public void configureWithOptions(ReadableMap params, final Callback onComplete) {
-    System.out.println("Calling configureWithOptions");
-    Log.i(TAG, "configureWithOptions");
+  public void configureWithOptions(ReadableMap params, @Nullable final Callback onComplete) {
+    Log.d(TAG, "configureWithOptions");
+
     ReactContext mCtx = getReactApplicationContext();
     FirebaseOptions.Builder builder = new FirebaseOptions.Builder();
 
+    if (params.hasKey("applicationId")) {
+      final String applicationId = params.getString("applicationId");
+      Log.d(TAG, "Setting applicationId from params " + applicationId);
+      builder.setApplicationId(applicationId);
+    }
     if (params.hasKey("apiKey")) {
-        builder.setApiKey(params.getString("apiKey"));
+      final String apiKey = params.getString("apiKey");
+      Log.d(TAG, "Setting API key from params " + apiKey);
+      builder.setApiKey(apiKey);
     }
     if (params.hasKey("gcmSenderID")) {
-        builder.setGcmSenderId(params.getString("gcmSenderID"));
+      final String gcmSenderID = params.getString("gcmSenderID");
+      Log.d(TAG, "Setting gcmSenderID from params" + gcmSenderID );
+      builder.setGcmSenderId(gcmSenderID);
     }
     if (params.hasKey("storageBucket")) {
-        builder.setStorageBucket(params.getString("storageBucket"));
+      final String storageBucket = params.getString("storageBucket");
+      Log.d(TAG, "Setting storageBucket from params" + storageBucket);
+      builder.setStorageBucket(storageBucket);
     }
     if (params.hasKey("databaseURL")) {
-        builder.setDatabaseUrl(params.getString("databaseURL"));
+      final String databaseURL = params.getString("databaseURL");
+      Log.d(TAG, "Setting databaseURL from params" + databaseURL);
+      builder.setDatabaseUrl(databaseURL);
     }
     if (params.hasKey("clientID")) {
-        builder.setApplicationId(params.getString("clientID"));
+      final String clientID = params.getString("clientID");
+      Log.d(TAG, "Setting clientID from params" + clientID);
+      builder.setApplicationId(clientID);
     }
 
     try {
         Log.i(TAG, "Configuring");
         FirebaseApp app = FirebaseApp.initializeApp(mCtx, builder.build());
         Log.i(TAG, "Configured");
-        onComplete.invoke();
+        onComplete.invoke(app);
     }
     catch (Exception ex){
         Log.e(TAG, "ERROR configureWithOptions");
         Log.e(TAG, ex.getMessage());
-        onComplete.invoke();
+        onComplete.invoke(ex.getMessage());
     }
   }
 
