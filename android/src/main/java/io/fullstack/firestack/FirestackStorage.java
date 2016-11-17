72d108583ad46fd4e2a4e6886ad1faaf74d7ea36
salakar <mike.diarmid@gmail.com>
2016-11-17 20:07:00 +0000

    cleanup imports

--- android/src/main/java/io/fullstack/firestack/FirestackStorage.java
+++ android/src/main/java/io/fullstack/firestack/FirestackStorage.java
@@ -1,41 +1,39 @@
 package io.fullstack.firestack;
 
+import android.util.Log;
 import android.os.Environment;
 import android.content.Context;
-import android.util.Log;
 
+import java.io.File;
 import java.util.Map;
 import java.util.HashMap;
-import java.io.File;
 
 import android.net.Uri;
-import android.provider.MediaStore;
 import android.database.Cursor;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 
-import com.facebook.react.bridge.Arguments;
-import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContextBaseJavaModule;
-import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
 
+import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-
-import com.google.firebase.storage.OnProgressListener;
-import com.google.firebase.storage.OnPausedListener;
 
 import com.google.firebase.FirebaseApp;
-
-import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.UploadTask;
-
+import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageMetadata;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.OnPausedListener;
+import com.google.firebase.storage.OnProgressListener;
+
 
 class FirestackStorageModule extends ReactContextBaseJavaModule {
 
