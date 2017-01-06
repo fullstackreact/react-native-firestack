dba8c11d15e9ac92efde92bb77e7ccaf8f85037f
Francisco Saldana <fvs@alum.mit.edu>
2017-01-06 00:24:33 -0600

    Resolve promise and send callback appropriate result.

--- ios/Firestack/FirestackDatabase.m
+++ ios/Firestack/FirestackDatabase.m
@@ -21,6 +21,7 @@
 @property FIRDatabaseHandle childRemovedHandler;
 @property FIRDatabaseHandle childMovedHandler;
 @property FIRDatabaseHandle childValueHandler;
++ (NSDictionary *) snapshotToDict:(FIRDataSnapshot *) snapshot;
 @end
 
 @implementation FirestackDBReference
@@ -46,7 +47,7 @@
 {
     if (![self isListeningTo:eventName]) {
         id withBlock = ^(FIRDataSnapshot * _Nonnull snapshot) {
-            NSDictionary *props = [self snapshotToDict:snapshot];
+            NSDictionary *props = [FirestackDBReference snapshotToDict:snapshot];
             [self sendJSEvent:DATABASE_DATA_EVENT
                         title:eventName
                         props: @{
@@ -74,7 +75,7 @@
 {
     [_query observeSingleEventOfType:FIRDataEventTypeValue
                            withBlock:^(FIRDataSnapshot * _Nonnull snapshot) {
-                               NSDictionary *props = [self snapshotToDict:snapshot];
+                               NSDictionary *props = [FirestackDBReference snapshotToDict:snapshot];
                                callback(@[[NSNull null], @{
                                               @"eventName": @"value",
                                               @"path": _path,
@@ -131,7 +132,7 @@
     [self unsetListeningOn:name];
 }
 
-- (NSDictionary *) snapshotToDict:(FIRDataSnapshot *) snapshot
++ (NSDictionary *) snapshotToDict:(FIRDataSnapshot *) snapshot
 {
     NSMutableDictionary *dict = [[NSMutableDictionary alloc] init];
     [dict setValue:snapshot.key forKey:@"key"];
@@ -485,8 +486,22 @@ RCT_EXPORT_METHOD(beginTransaction:(NSString *) path
                 currentData.value = value;
                 return [FIRTransactionResult successWithValue:currentData];
             }
-        } andCompletionBlock:^(NSError * _Nullable error, BOOL committed, FIRDataSnapshot * _Nullable snapshot) {
-            [self handleCallback:@"transaction" callback:onComplete databaseError:error];
+        } andCompletionBlock:^(NSError * _Nullable databaseError, BOOL committed, FIRDataSnapshot * _Nullable snapshot) {
+            if (databaseError != nil) {
+                NSDictionary *evt = @{
+                                      @"errorCode": [NSNumber numberWithInt:[databaseError code]],
+                                      @"errorDetails": [databaseError debugDescription],
+                                      @"description": [databaseError description]
+                                      };
+                onComplete(@[evt]);
+            } else {
+                onComplete(@[[NSNull null], @{
+                               @"committed": [NSNumber numberWithBool:committed],
+                               @"snapshot": [FirestackDBReference snapshotToDict:snapshot],
+                               @"status": @"success",
+                               @"method": @"transaction"
+                               }]);
+            }
         } withLocalEvents:applyLocally];
     });
 }
